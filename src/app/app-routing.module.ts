b38aa4b8082b6ecd9145928b7f514a0be3599f1a
AlexRGB <alexismtz200326@gmail.com>
2024-01-16 17:53:31 -0600

    Desestructuracion en componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NovedadesComponent } from './pages/novedades/novedades.component';
+import { LanzamientoComponent } from './pages/lanzamiento/lanzamiento.component';
+import { PlanesComponent } from './pages/planes/planes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'newsletter', component: NovedadesComponent },
+  { path: 'lanzamiento', component: LanzamientoComponent },
+  { path: 'planes', component: PlanesComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/lanzamiento/lanzamiento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lanzamiento',
+  templateUrl: './lanzamiento.component.html',
+  styleUrls: ['./lanzamiento.component.scss']
+})
+export class LanzamientoComponent {
+
+}
